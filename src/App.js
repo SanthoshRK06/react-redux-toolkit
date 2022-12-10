48e6234c0a2f925cdbb2cee3763905fbf1bf6ea2
Santhosh Kumar <santhoshrk061297@gmail.com>
2022-12-10 17:45:09 +0530

    version1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PostsList from "./features/posts/PostsList.js";
+import AddPostForm from "./features/posts/AddPostForm.js";
+
+function App() {
+  return (
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
+}
+
+export default App;
